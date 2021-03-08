9b08628d011875601efd21887d99fa39656f4693
August Thorell <augustthorell@gmail.com>
2021-03-08 11:42:34 +0100

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `August TracisCI test project`,
+    description: `I will try to understand as much as possible of the lecture.`,
     author: `@gatsbyjs`,
   },
   plugins: [
